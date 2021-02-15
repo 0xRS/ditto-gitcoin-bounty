d83275bf36da4eeafb171c21bd9324676d6414f6
0xRS <40622223+0xRS@users.noreply.github.com>
2021-02-15 09:54:04 +0530

    prompt_toolkit for cui

--- export_swap_data.py
+++ export_swap_data.py
@@ -8,9 +8,16 @@ from prettytable import PrettyTable
 from dotenv import load_dotenv
 from os import getenv
 
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.lexers import PygmentsLexer
+from prompt_toolkit.styles import Style
+from pygments.lexers.solidity import SolidityLexer
+from prompt_toolkit import print_formatted_text, HTML
+
 infura_id = "60e52c2fc2764ffab5385cc6342fca3a"
 
-def print_deposit_events(ditto_contract, fromBlock, toBlock):
+def print_deposit_events(ditto_contract, fromBlock, toBlock, session):
     events = ditto_contract.events.SwapDeposit.getLogs(fromBlock=fromBlock, toBlock=toBlock)
     f = open(str(fromBlock)+"-"+str(toBlock)+'.csv', 'w')
     writer = csv.writer(f)
@@ -29,6 +36,8 @@ def print_deposit_events(ditto_contract, fromBlock, toBlock):
         x.add_row(row)
     f.close()
     print(x)
+    print_formatted_text(HTML("<aaa fg='ansiwhite' bg='ansigreen'>&#2714; Saved to "+str(fromBlock)+"-"+str(toBlock)+".csv</aaa>"))
+
 
 
 def main():
@@ -37,7 +46,31 @@ def main():
     w3r = Web3(Web3.HTTPProvider('https://ropsten.infura.io/v3/'+infura_id))
     w3b = Web3(Web3.HTTPProvider('https://bsc-dataseed.binance.org/'))
     ditto_contract = w3r.eth.contract(address=contract_address, abi=abi)
-    print_deposit_events(ditto_contract, 1, 1000000000)
+    fromBlock = 1
+    toBlock = 1000000000
+    completer = WordCompleter(['historical swaps', 'real time swaps'])
+    style = Style.from_dict({
+        'completion-menu.completion': 'bg:#008888 #ffffff',
+        'completion-menu.completion.current': 'bg:#00aaaa #000000',
+        'scrollbar.background': 'bg:#88aaaa',
+        'scrollbar.button': 'bg:#222222',
+    })
+    session = PromptSession(
+        lexer=PygmentsLexer(SolidityLexer), completer=completer, style=style)
+
+    while True:
+        try:
+            text = session.prompt('> ')
+            print(text)
+            if (text == 'historical swaps'):
+                startBlock = session.prompt(HTML('> &#9658; Start Block: '))
+                endBlock = session.prompt(HTML('> &#9658; End Block: '))
+                print_deposit_events(ditto_contract, int(startBlock), int(endBlock), session)
+        except KeyboardInterrupt:
+            continue  # Control-C pressed. Try again.
+        except EOFError:
+            break  # Control-D pressed.
+    # print_deposit_events(ditto_contract, 1, 1000000000)
 
 if __name__ == '__main__':
     main()
